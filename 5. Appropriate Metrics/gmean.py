cd82ddcc0787777b55555b9996e78b34d0a13b13
Albert <maxiaoscut@aliyun.com>
2019-01-23 12:03:55 +0800

    Update gmean.py

--- 5. Appropriate Metrics/gmean.py
+++ 5. Appropriate Metrics/gmean.py
@@ -3,7 +3,7 @@
 # Github：https://github.com/Albertsr
 
 import numpy as np
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, make_scorer
 
 
 def gmean(y_true, y_pred):
@@ -11,3 +11,7 @@ def gmean(y_true, y_pred):
     recall = tp/(tp+fn)
     specificity = tn/(tn+fp)
     return np.sqrt(recall * specificity)
+
+# 通过工厂函数make_scorer生成gmean_score，可直接用于网格搜索进行调参
+# 形如：sklearn.model_selection.GridSearchCV(estimator, param_grid, scoring=gmean_score）
+gmean_score = make_scorer(gmean, greater_is_better=True)
